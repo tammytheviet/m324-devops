944e26041227981b8d58af72bb1844ab111ae48a
Fabo <fabiogabriel@hotmail.ch>
2023-04-05 10:55:36 +0200

    Added versioning to PostMapping

--- backend/src/main/java/com/example/demo/TaskController.java
+++ backend/src/main/java/com/example/demo/TaskController.java
@@ -29,11 +29,18 @@ public class TaskController {
     }
 
     @CrossOrigin
-    @PostMapping// Map ONLY Post Requests
-    public ResponseEntity<String> addTask(@Valid @RequestBody TodoRequest taskRequest) {
+    @PostMapping("/v1")// Map ONLY Post Requests
+    public ResponseEntity<String> addTaskV1(@Valid @RequestBody TodoRequest taskRequest) {
             System.out.println("API EP '/add': '" + taskRequest.getTaskdescription() + "'");
             taskRepository.save(new Task(taskRequest.getTaskdescription()));
             return ResponseEntity.ok("redirect/");
+    }
+
+    @CrossOrigin
+    @PostMapping("/v2")// Map ONLY Post Requests
+    public ResponseEntity<String> addTaskV2(@Valid @RequestBody TodoRequest taskRequest) {
+            taskRepository.save(new Task(taskRequest.getTaskdescription()));
+            return ResponseEntity.ok("Added new Task");
     }
 
     /*
